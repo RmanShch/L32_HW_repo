18d6acb1cb506e3a8d3aa138ad3af3ad10b5ebab
RmanShch <man46ro@gmail.com>
2022-02-08 19:45:17 +0300

    initial commit

---
+++ Roman_Shcherbitsky_HW_L32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roman_Shcherbitsky_HW_L32
+//
+//  Created by Анна Шербицкая on 8.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
